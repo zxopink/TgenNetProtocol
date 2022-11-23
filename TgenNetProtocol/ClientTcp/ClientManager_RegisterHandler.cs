44fcc5f08709efae2b3dd632f1734bd01ad89bc9
Yoav Haik <yoavhaik@gmail.com>
2022-11-23 16:15:04 +0200

    put unused methods into comments

--- TgenNetProtocol/ClientTcp/ClientManager_RegisterHandler.cs
+++ TgenNetProtocol/ClientTcp/ClientManager_RegisterHandler.cs
@@ -10,13 +10,13 @@ namespace TgenNetProtocol
 
         public void Register<T>(Action<T> method) =>
             MethodRegister.Register(method);
-        private void Register(Delegate meth) =>
-            MethodRegister.Register(meth);
+        //private void Register(Delegate meth) =>
+        //    MethodRegister.Register(meth);
 
         public void Unregister<T>(Action<T> method) =>
             MethodRegister.Unregister(method);
-        private bool Unregister(Delegate meth) =>
-            MethodRegister.Unregister(meth);
+        //private bool Unregister(Delegate meth) =>
+        //    MethodRegister.Unregister(meth);
 
 
         private void CallRegisters(object message) =>
